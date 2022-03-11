0a8b8ff3fa1e1007f6a7800b44db8cc66305f90d
Heviane Bastos <heviane@gmail.com>
2022-03-11 19:39:48 -0300

    Review Introduction and Basic Sintaxe

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,35 @@
-var test = 'Escopo global';
-console.log(test);
+let arr = [1, 2, 3, 4, 5];
 
-function fn(){
-    var fnTest = 'Escopo local';
-    console.log(fnTest);
-}
-fn();
+// shift()  remove no inicio
+arr.shift();
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+// unshift()  add item no inicio
+arr.unshift(7);
+console.log(arr);
+arr.push(8);
+console.log(arr);
+
+
+// var scoreboard;
+// var player1 = 5;
+// var player2 = 3;
+// switch(scoreboard){
+//     case (scoreboard = ((player1 > player2) ? player1 : player2)):
+//         console.log("Player 1 wins!");
+//         break;
+//     case scoreboard = (player2 > player1):
+//         console.log("Player 2 wins!");
+//         break;
+//     case scoreboard = 0: 
+//         console.log("testing...0");
+//         break;
+//     case scoreboard = 1:
+//         console.log("testing...1");
+//         break;
+//     default:
+//         console.log("Try again, no one won!");
+//         break;
+// }
